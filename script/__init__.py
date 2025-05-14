62a23f4770f14b09879600f8833eb126652af4f6
LOYINuts <loyinuts@aliyun.com>
2025-05-14 15:17:37 +0800

    2025-05-14

--- script/__init__.py
+++ script/__init__.py
@@ -1,7 +1,7 @@
 from .model import *
-from .CNN_model import *
-from .linear_model import *
+from .model_V2 import *
+from .model_V1 import *
 from .CBAM import *
 from .adjust_learning import *
 from .dataloader import *
-from .model_inter import *
+# from .model_inter import *
